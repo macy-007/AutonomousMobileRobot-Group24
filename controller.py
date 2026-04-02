46d81d0e0e03d5b819bacda36fb7888f8be04268
Ewan <ewan.briggs@virginmedia.com>
2026-04-02 17:37:32 +0100

    Copied Controller.py code over for Role 3 (Includes Inner Loop already imported)

---
+++ controller.py
@@ -0,0 +1,20 @@
+# wind_flag = False
+# Implement a controller
+
+import numpy as np
+from InnerLoopController import InnerLoopController
+
+inner_controller = InnerLoopController()
+
+def controller(state, target_pos, dt, wind_enabled=False):
+    # state format: [position_x (m), position_y (m), position_z (m), roll (radians), pitch (radians), yaw (radians)]
+    # target_pos format: (x (m), y (m), z (m), yaw (radians))
+    # dt: time step (s)
+    # wind_enabled: boolean flag to indicate if wind disturbance should be considered in the control algorithm
+
+    current_pos = np.array(state[0:3])
+    current_yaw = state[5]
+    # # return velocity command format: (velocity_x_setpoint (m/s), velocity_y_setpoint (m/s), velocity_z_setpoint (m/s), yaw_rate_setpoint (radians/s))
+    output = (0, 0, 0, 0)
+
+    return output
